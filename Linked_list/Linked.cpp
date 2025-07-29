fd74d3a97a64b8f76b2179a82256051394336554
Rachdy <ismailrchdy123@gmail.com>
2025-07-29 18:12:16 +0200

    write some function for Linked List

--- Linked_list/Linked.cpp
+++ Linked_list/Linked.cpp
@@ -1,8 +1,43 @@
-#include<iostream>
-#include<string>
+// Linked_List.cpp
+// 
+#include <iostream>
+#include "List.h"
 
-class Item{
-	class  Item Next = nullptr;
-	std::string data ="";
 
+//template<typename T> struct List;
+
+template<typename T> 
+void List<T>::print_list(List<T>* Head) {
+	if (Head == nullptr) return ;
+	List<T>* temp;
+
+	temp = Head;
+	while (temp != nullptr) {
+		std::cout <<temp->data << "\n";
+		temp = temp->next;
+
+	}
+}
+
+template<typename T>
+void List<T>::delete_list(List<T>* Head) {
+	List<T>* temp;
+	List<T>* prev;
+	temp = prev = Head;
+
+	while (temp != nullptr) {
+		prev = temp;
+		temp = temp->next;
+		delete prev;
+	}
+}
+
+template<typename T>
+List<T>* List<T>::add_front(List<T>* Head, T value) {
+
+	List<T>* NewNode = new List<T>(value);
+	if (Head != nullptr) {
+		NewNode->next = Head;
+	}
+	return NewNode;
 }
--- Linked_list/Linked.hpp
+++ Linked_list/Linked.hpp
@@ -5,11 +5,13 @@ template<typename T>
 struct List {
 	List* next = nullptr;
 	T data;
-	void  print_list(List Head);
-	void  delete_list(List Head);
-	List* add_front(List Head, T value);
-	List* add_end(List Head, T value);
-	List* delete_atposition(List Head, T value);
+	List(T value) : data(value), next(nullptr) {}
+	void  print_list(List<T>* Head);
+	void  delete_list(List<T>* Head);
+	List<T>* add_front(List<T>* Head, T value);
+	List<T>* add_end(List<T>* Head, T value);
+	List<T> delete_atposition(List<T>* Head, T value);
 };
 
 #endif // !"LIST_HPP"
+
