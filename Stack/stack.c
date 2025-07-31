bad0be9f70dbeb65b833e04a61a3d055728e1301
Rachdy <ismailrchdy123@gmail.com>
2025-07-31 14:56:55 +0200

    fix the issue  of negative size of items

--- Stack/stack.c
+++ Stack/stack.c
@@ -2,56 +2,60 @@
 #include "stdlib.h"
 #include "stack.h"
 
-
 struct Stack;
 
 // Function to initialize the stack
 
-struct Stack* stack_create(int stack_size)
+struct Stack *stack_create(size_t stack_size)
 {
-	Stack* S = (Stack*)malloc(sizeof(Stack));
-	if (!S) return NULL;//check Memory allocation for Stack S 
+	Stack *S = (Stack *)malloc(sizeof(Stack));
+	if (!S)
+		return NULL; // check Memory allocation for Stack S
 	S->size = stack_size;
-	S->items = (int*)malloc(stack_size * sizeof(int));
-	if (!S->items) {//check Memory allocation  for items
+	S->items = (int *)malloc(stack_size * sizeof(int));
+	if (!S->items)
+	{ // check Memory allocation  for items
 		printf("Memory allocation failed for items!\n");
 		free(S);
 		return NULL;
 	}
-	S->top = -1;
+	S->top = 0; // the index start counting from  1 , when top = 0 means no element are inside the stack
 
 	return S;
 }
 
 // return true id stack is empty and false when not
-bool is_Empty( Stack* S)
+bool is_Empty(Stack *S)
 {
-	if (S->top == -1)
-	{ // if the top = -1 means no value are inside the stack
-		return true;// return true if the stack is empty
+	if (S->top == 0)
+	{				 // if the top = 0 means no value are inside the stack
+		return true; // return true if the stack is empty
 	}
 	else
 	{
-		return false;// return false when the stack is not empty
+		return false; // return false when the stack is not empty
 	}
 }
 
-
-//function to know how much elements are inside the Stack
-int size_of_stack(Stack* S) {
+// function to know how much elements are inside the Stack
+int size_of_stack(Stack *S)
+{
 	return S->top;
 }
 
-//check if the Stack is full or not
-bool is_full(Stack* S) {
-	if (S->top == S->size - 1) return true;
-	else return false;
+// check if the Stack is full or not
+bool is_full(Stack *S)
+{
+	if (S->top == S->size)
+		return true;
+	else
+		return false;
 }
 
 // Push new value into Stack
-bool stack_push( Stack* S, int value)
+bool stack_push(Stack *S, int value)
 {
-	if (S->top < S->size -1)
+	if (S->top < S->size)
 	{			  // check if stack if full when not push the new value
 		S->top++; // increment the top
 		S->items[S->top] = value;
@@ -60,60 +64,67 @@ bool stack_push( Stack* S, int value)
 	return false;
 }
 // delete the stack from memory
-void stack_delete(Stack* S)
+void stack_delete(Stack *S)
 {
+	free(S->items);
 	free(S);
 }
 
 // delete the last element
-int stack_pop(Stack* S)
+bool stack_pop(Stack *S)
 {
 	if (is_Empty(S))
 	{ // when the stack is empty return 0 as default value
-		return -1;
+		return false;
 	}
 	int element = S->items[S->top];
 	S->top--;
-	return element;
+	return true;
 }
 
-//expand the Stack if it is  full
-bool expand_stack(Stack *S) {
-	if (is_full(S)) {//check if the stack is full 
-		//if yes expand the size of stack
-		int* temp = (int*)realloc(S->items, (S->size * 1));
+// expand the Stack if it is  full
+bool expand_stack(Stack *S)
+{
+	if (is_full(S))
+	{ // check if the stack is full
+		// if yes expand the size of stack
+		int *temp = (int *)realloc(S->items, (S->size * 2));
 
-		//check if Memory allocation for temp is successful
-		if (temp) {
+		// check if Memory allocation for temp is successful
+		if (temp)
+		{
 			S->items = temp;
-			return true;// return when Memory allocation for temp is successful
+			return true; // return when Memory allocation for temp is successful
 		}
-		else {
-			return false; //return false when Memory allocation faild
+		else
+		{
+			return false; // return false when Memory allocation faild
 		}
 	}
-	else return false;// return false if the stack is not full
-
+	else
+		return false; // return false if the stack is not full
 }
 
-// function to clear the stack 
+// function to clear the stack
 
-bool clear_stack(Stack *S) {
+bool clear_stack(Stack *S)
+{
 
-	if (is_Empty) {
+	if (is_Empty)
+	{
 		return false; // the Stack is allready empty
 	}
-	else {
+	else
+	{
 		free(S->items);
 
-		S->items = (int*)malloc(S->size * sizeof(int));
-		if (!S->items) {//check Memory allocation  for items is successful
+		S->items = (int *)malloc(S->size * sizeof(int));
+		if (!S->items)
+		{ // check Memory allocation  for items is successful
 			printf("Memory allocation failed for items!\n");
 			free(S);
 			return false;
 		}
 		return true;
 	}
-
 }
-
--- Stack/stack.h
+++ Stack/stack.h
@@ -1,15 +1,15 @@
 #ifndef __STACK_H__
 #define __STACK_H__
-#include"stdbool.h"
+#include "stdbool.h"
 
 typedef struct
 {
-	int top;
-	int* items;
-	int size;
-} Stack;								  // forward declaration
-struct Stack* stack_create(int size_stack); // create  new Stack
-bool stack_push(struct Stack*, int);	  // push new element into stack
-int stack_pop(struct Stack*);			  // delet last element
-void stack_delete(struct Stack*);		  // Delete the Stack
+	unsigned int top;
+	int *items;
+	size_t size;
+} Stack;									   // forward declaration
+struct Stack *stack_create(size_t stack_size); // create  new Stack
+bool stack_push(struct Stack *, int);		   // push new element into stack
+int stack_pop(struct Stack *);				   // delet last element
+void stack_delete(struct Stack *);			   // Delete the Stack
 #endif
